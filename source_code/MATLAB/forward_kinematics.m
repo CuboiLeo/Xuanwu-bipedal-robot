ea225cd6a3767654a6ea03792c82d7e2ea03a03a
Leo Liu <65527170+CuboiLeo@users.noreply.github.com>
2024-12-01 20:36:05 -0500

    refactor(MATLAB): added the method of Product of Exponentials for kinematics and tested the code

--- source_code/MATLAB/forward_kinematics.m
+++ source_code/MATLAB/forward_kinematics.m
@@ -1,5 +1,85 @@
+%% Product of Exponentials (for V2 robot)
 clear; clc; close all;
+syms theta1 theta2 theta3 theta4 theta6 theta7 theta8 theta9 L1 L2 L3 L4 L5 real
 
+% angular velocities
+w1 = [0 0 -1]';
+w2 = [0 1 0]';
+w3 = [1 0 0]';
+w4 = [1 0 0]';
+
+w6 = [0 0 -1]';
+w7 = [0 1 0]';
+w8 = [1 0 0]';
+w9 = [1 0 0]';
+
+% point on the axis
+q1 = [-L1 0 0]';
+q2 = [-L1 -L3 -L2]';
+q3 = [-L1 0 -L2]';
+q4 = [-L1 0 -(L2+L4)]';
+
+q6 = [L1 0 0]';
+q7 = [L1 -L3 -L2]';
+q8 = [L1 0 -L2]';
+q9 = [L1 0 -(L2+L4)]';
+
+% linear velocity
+v1 = -cross(w1,q1);
+v2 = -cross(w2,q2);
+v3 = -cross(w3,q3);
+v4 = -cross(w4,q4);
+
+v6 = -cross(w6,q6);
+v7 = -cross(w7,q7);
+v8 = -cross(w8,q8);
+v9 = -cross(w9,q9);
+
+% screw axes
+W = {w1,w2,w3,w4,0,w6,w7,w8,w9};
+V = {v1,v2,v3,v4,0,v6,v7,v8,v9};
+for i = [1:4,6:9]
+    S{i} = [0 -W{i}(3) W{i}(2) V{i}(1);
+            W{i}(3) 0 -W{i}(1) V{i}(2);
+            -W{i}(2) W{i}(1) 0 V{i}(3);
+               0         0   0    0   ];
+end
+
+% end-effector frame configuration
+M_left = [1 0 0 -L1;
+     0 1 0 0;
+     0 0 1 -(L2+L4+L5);
+     0 0 0 1];
+M_right = [1 0 0 L1;
+     0 1 0 0;
+     0 0 1 -(L2+L4+L5);
+     0 0 0 1];
+% final transformation matrix
+T_left = expm(S{1}*theta1)*expm(S{2}*theta2)*expm(S{3}*theta3)*expm(S{4}*theta4)*M_left;
+T_right = expm(S{6}*theta6)*expm(S{7}*theta7)*expm(S{8}*theta8)*expm(S{9}*theta9)*M_right;
+
+% value substitution
+val_theta1 = 0;
+val_theta2 = pi/2;
+val_theta3 = 0;
+val_theta4 = 0;
+val_theta6 = 0;
+val_theta7 = pi/2;
+val_theta8 = 0;
+val_theta9 = 0;
+val_L1 = 0.135;
+val_L2 = 0.12;
+val_L3 = 0.09;
+val_L4 = 0.18;
+val_L5 = 0.27;
+sub_vals = [val_theta1 val_theta2 val_theta3 val_theta4 val_theta6 val_theta7 val_theta8 val_theta9 val_L1 val_L2 val_L3 val_L4 val_L5];
+sub_syms = [theta1 theta2 theta3 theta4 theta6 theta7 theta8 theta9 L1 L2 L3 L4 L5];
+
+val_T_left = vpa(subs(T_left,sub_syms,sub_vals),2)
+val_T_right = vpa(subs(T_right,sub_syms,sub_vals),2)
+
+%% Modified DH Method (for V1 robot)
+clear; clc; close all;
 syms theta1 theta2 theta3 theta4;
 syms a1 a2 a3 a4 a5 alpha2 alpha3;
 % theta d a alpha
@@ -12,7 +92,7 @@ num_T_matrix = size(dhparams,1);
 T = sym(zeros(4, 4, num_T_matrix));
 T_05 = sym(eye(4));
 for i = 1:5
-    T(:,:,i) = [cos(dhparams (i,1)) -sin(dhparams(i,1)) 0 dhparams(i,3);
+    T(:,:,i) = [cos(dhparams(i,1)) -sin(dhparams(i,1)) 0 dhparams(i,3);
         sin(dhparams(i,1))*cos(dhparams(i,4)) cos(dhparams(i,1))*cos(dhparams(i,4)) -sin(dhparams(i,4)) -dhparams(i,2)*sin(dhparams(i,4));
         sin(dhparams(i,1))*sin(dhparams(i,4)) cos(dhparams(i,1))*sin(dhparams(i,4)) cos(dhparams(i,4)) dhparams(i,2)*cos(dhparams(i,4));
         0 0 0 1];
@@ -35,6 +115,3 @@ val_y = vpa(subs(y,[sym_theta sym_sub],[val_theta val_sub]),2)
 val_z = vpa(subs(z,[sym_theta sym_sub],[val_theta val_sub]),2)
 val_yaw = vpa(subs(yaw,[sym_theta sym_sub],[val_theta val_sub]),2)
 %T_05 = subs(T_05,{theta1 theta2 theta3 theta4 a1 a2 a3 a4 a5 alpha2 alpha3},{0 pi/2 0 0 -0.135 -0.095 -0.09 -0.18 -0.38 -pi/2 -pi/2})
-
-
-
--- source_code/MATLAB/inverse_kinematics.m
+++ source_code/MATLAB/inverse_kinematics.m
@@ -1,5 +1,109 @@
+%% Product of Exponentials (for V2 robot)
 clear; clc; close all;
+syms theta1 theta2 theta3 theta4 theta6 theta7 theta8 theta9 L1 L2 L3 L4 L5 real
 
+% angular velocities
+w1 = [0 0 -1]';
+w2 = [0 1 0]';
+w3 = [1 0 0]';
+w4 = [1 0 0]';
+
+w6 = [0 0 -1]';
+w7 = [0 1 0]';
+w8 = [1 0 0]';
+w9 = [1 0 0]';
+
+% point on the axis
+q1 = [-L1 0 0]';
+q2 = [-L1 -L3 -L2]';
+q3 = [-L1 0 -L2]';
+q4 = [-L1 0 -(L2+L4)]';
+
+q6 = [L1 0 0]';
+q7 = [L1 -L3 -L2]';
+q8 = [L1 0 -L2]';
+q9 = [L1 0 -(L2+L4)]';
+
+% linear velocity
+v1 = -cross(w1,q1);
+v2 = -cross(w2,q2);
+v3 = -cross(w3,q3);
+v4 = -cross(w4,q4);
+
+v6 = -cross(w6,q6);
+v7 = -cross(w7,q7);
+v8 = -cross(w8,q8);
+v9 = -cross(w9,q9);
+
+% screw axes
+W = {w1,w2,w3,w4,0,w6,w7,w8,w9};
+V = {v1,v2,v3,v4,0,v6,v7,v8,v9};
+for i = [1:4,6:9]
+    S{i} = [0 -W{i}(3) W{i}(2) V{i}(1);
+            W{i}(3) 0 -W{i}(1) V{i}(2);
+            -W{i}(2) W{i}(1) 0 V{i}(3);
+               0         0   0    0   ];
+end
+
+% end-effector frame configuration
+M_left = [1 0 0 -L1;
+     0 1 0 0;
+     0 0 1 -(L2+L4+L5);
+     0 0 0 1];
+M_right = [1 0 0 L1;
+     0 1 0 0;
+     0 0 1 -(L2+L4+L5);
+     0 0 0 1];
+% final transformation matrix
+T_left = simplify(expm(S{1}*theta1)*expm(S{2}*theta2)*expm(S{3}*theta3)*expm(S{4}*theta4)*M_left);
+T_right = expm(S{6}*theta6)*expm(S{7}*theta7)*expm(S{8}*theta8)*expm(S{9}*theta9)*M_right;
+
+% value substitution
+val_theta1 = 0;
+val_theta2 = 0.0001;
+val_theta3 = 0.0001;
+val_theta4 = 0.0001;
+val_theta6 = 0;
+val_theta7 = pi/2;
+val_theta8 = 0;
+val_theta9 = 0;
+val_L1 = 0.135;
+val_L2 = 0.12;
+val_L3 = 0.09;
+val_L4 = 0.18;
+val_L5 = 0.27;
+sub_vals = [val_theta1 val_theta6 val_theta7 val_theta8 val_theta9 val_L1 val_L2 val_L3 val_L4 val_L5];
+sub_syms = [theta1 theta6 theta7 theta8 theta9 L1 L2 L3 L4 L5];
+
+% coordinate vector
+x = T_left(1,4);
+y = T_left(2,4);
+z = T_left(3,4);
+sym_X_act = [x;y;z];
+X_ref = [-0.13;0.01;-0.55];
+
+% Newton Raphson Method
+sym_theta = [theta2 theta3 theta4];
+val_theta = [val_theta2 val_theta3 val_theta4];
+
+norm_e = 1;
+epsilon = 0.005;
+iteration = 0;
+jacob = jacobian(sym_X_act,sym_theta);
+while norm_e > epsilon && iteration < 100
+    val_X_act = vpa(subs(sym_X_act,[sym_theta sub_syms],[val_theta sub_vals]),8);
+    error_X = vpa(X_ref - val_X_act,8);
+    norm_e = norm(error_X)
+    val_jacob = vpa(subs(jacob,[sym_theta sub_syms],[val_theta sub_vals]),8);
+    inv_jacob = inv(val_jacob);
+    delta_theta = val_jacob\error_X;
+    val_theta = val_theta + delta_theta';
+    val_theta = mod(eval(val_theta) + pi, 2*pi) - pi
+    iteration = iteration + 1;
+end
+
+%% Modified DH Method (for V1 robot)
+clear; clc; close all;
 syms theta1 theta2 theta3 theta4;
 syms a1 a2 a3 a4 a5 alpha2 alpha3;
 % theta d a alpha
@@ -29,11 +133,11 @@ sym_theta = [theta2 theta3 theta4];
 sym_sub = [a1 a2 a3 a4 a5 alpha2 alpha3];
 val_sub = [-0.135 -0.095 -0.09 -0.18 -0.38 -pi/2 -pi/2];
 
-X_ref = [-0.23;0.001;0.60 ];
+X_ref = [-0.23;0.001;0.60];
 val_theta = [pi/2;0.001;0.001];
 epsilon = 1;
 error_X = 1;
-iteration = 0
+iteration = 0;
 while norm(error_X) > 0.002 && iteration < 100
     val_X_act = vpa(subs(sym_X_act,[sym_theta sym_sub theta1],[val_theta' val_sub 0]),2);
     jacob = vpa(subs(jacobian(sym_X_act,sym_theta),[sym_theta sym_sub theta1],[val_theta' val_sub 0]),2);
@@ -43,6 +147,7 @@ while norm(error_X) > 0.002 && iteration < 100
     norm(error_X)
     val_theta = val_theta + delta_theta;
     epsilon = abs(norm(eval(delta_theta)))/abs(norm(eval(val_theta)));
+    iteration = iteration + 1;
 end
 
 
