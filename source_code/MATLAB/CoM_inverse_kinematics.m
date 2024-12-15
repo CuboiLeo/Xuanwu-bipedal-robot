5b165ad40a08b67896624d8e84513c414056c61f
Leo Liu <65527170+CuboiLeo@users.noreply.github.com>
2024-12-15 00:18:12 -0500

    fix(MATLAB): fixed CoM forward and inverse kinematics

--- source_code/MATLAB/CoM_inverse_kinematics.m
+++ source_code/MATLAB/CoM_inverse_kinematics.m
@@ -1,17 +1,17 @@
-%% Product of Exponentials (for V2 robot)
+%% for V2 robot
 clear; clc; close all;
-syms theta1 theta2 theta3 theta4 theta6 theta7 theta8 theta9 L1 L2 L3 L4 L5 real
+syms theta1 theta2 theta3 theta4 theta6 theta7 theta8 theta9 L1 L2 L3 L4 L5 m1 m2 m3 m4 m5 real
 
 % angular velocities
-w1 = [0 0 1]';
-w2 = [0 -1 0]';
-w3 = [-1 0 0]';
-w4 = [-1 0 0]';
+w1 = [0 0 -1]';
+w2 = [0 1 0]';
+w3 = [1 0 0]';
+w4 = [1 0 0]';
 
-w6 = [0 0 1]';
-w7 = [0 -1 0]';
-w8 = [-1 0 0]';
-w9 = [-1 0 0]';
+w6 = [0 0 -1]';
+w7 = [0 1 0]';
+w8 = [1 0 0]';
+w9 = [1 0 0]';
 
 % point on the axis
 q1 = [-L1 0 0]';
@@ -45,26 +45,12 @@ for i = [1:4,6:9]
                0         0   0    0   ];
 end
 
-% end-effector frame configuration (basically the base frame)
-M_left = [1 0 0 0;
-     0 1 0 0;
-     0 0 1 0;
-     0 0 0 1];
-M_right = [1 0 0 0;
-     0 1 0 0;
-     0 0 1 0;
-     0 0 0 1];
-
-% final transformation matrix
-T_left = simplify(M_left*expm(S{4}*theta4)*expm(S{3}*theta3)*expm(S{2}*theta2)*expm(S{1}*theta1));
-T_right = simplify(M_right*expm(S{9}*theta9)*expm(S{8}*theta8)*expm(S{7}*theta7)*expm(S{6}*theta6));
-
-% value substitution
-val_theta1 = 0;
+% substitute values
+val_theta1 = 0.0001;
 val_theta2 = 0.0001;
 val_theta3 = 0.0001;
 val_theta4 = 0.0001;
-val_theta6 = 0;
+val_theta6 = 0.0001;
 val_theta7 = 0.0001;
 val_theta8 = 0.0001;
 val_theta9 = 0.0001;
@@ -73,83 +59,118 @@ val_L2 = 0.12;
 val_L3 = 0.09;
 val_L4 = 0.18;
 val_L5 = 0.27;
-sub_vals = [val_theta1 val_theta6  val_L1 val_L2 val_L3 val_L4 val_L5];
-sub_syms = [theta1 theta6 L1 L2 L3 L4 L5];
+val_m1 = 1.73;
+val_m2 = 0.55;
+val_m3 = 0.38;
+val_m4 = 0.53;
+val_m5 = 0.42;
 
-sym_theta_left = [theta2 theta3 theta4];
-val_theta_left = [val_theta2 val_theta3 val_theta4];
-sym_theta_right = [theta7 theta8 theta9];
-val_theta_right = [val_theta7 val_theta8 val_theta9];
+% link's relative offset from the last joint
+syms C11 C12 C13 C21 C22 C23 C31 C32 C33 C41 C42 C43 C51 C52 C53
+C1 = [C11 C12 C13];
+C2 = [C21 C22 C23];
+C3 = [C31 C32 C33];
+C4 = [C41 C42 C43];
+C5 = [C51 C52 C53];
+val_C1 = [0 0 0.027];
+val_C2 = [-0.0075 -0.06 -0.067];
+val_C3 = [0 0.084 0];
+val_C4 = [0 0 -0.142];
+val_C5 = [0 0 -0.131];
 
-% coordinate vector
-x_left = T_left(1,4);
-y_left = T_left(2,4);
-z_left = T_left(3,4);
-sym_X_act_left = [x_left;y_left;z_left];
+sub_vals = [val_theta1 val_theta6 val_L1 val_L2 val_L3 val_L4 val_L5 val_C1 val_C2 val_C3 val_C4 val_C5 val_m1 val_m2 val_m3 val_m4 val_m5];
+sub_syms = [theta1 theta6 L1 L2 L3 L4 L5 C1 C2 C3 C4 C5 m1 m2 m3 m4 m5];
 
-x_right = T_right(1,4);
-y_right = T_right(2,4);
-z_right = T_right(3,4);
-sym_X_act_right = [x_right;y_right;z_right];
+M1 = [1 0 0 C1(1);
+    0 1 0 C1(2);
+    0 0 1 C1(3);
+    0 0 0 1];
+M2 = [1 0 0 -L1+C2(1);
+    0 1 0 C2(2);
+    0 0 1 C2(3);
+    0 0 0 1];
+M3 = [1 0 0 -L1+C3(1);
+    0 1 0 -L3+C3(2);
+    0 0 1 -L2+C3(3);
+    0 0 0 1];
+M4 = [1 0 0 -L1+C4(1);
+    0 1 0 C4(2);
+    0 0 1 -L2+C4(3);
+    0 0 0 1];
+M5 = [1 0 0 -L1+C5(1);
+    0 1 0 C5(2);
+    0 0 1 -(L2+L4)+C5(3);
+    0 0 0 1];
+M6 = [1 0 0 L1-C2(1);
+    0 1 0 C2(2);
+    0 0 1 C2(3);
+    0 0 0 1];
+M7 = [1 0 0 L1+C3(1);
+    0 1 0 -L3+C3(2);
+    0 0 1 -L2+C3(3);
+    0 0 0 1];
+M8 = [1 0 0 L1+C4(1);
+    0 1 0 C4(2);
+    0 0 1 -L2+C4(3);
+    0 0 0 1];
+M9 = [1 0 0 L1+C5(1);
+    0 1 0 C5(2);
+    0 0 1 -(L2+L4)+C5(3);
+    0 0 0 1];
+T1 = M1;
+T2 = simplify(expm(S{1}*theta1)*M2);
+T3 = simplify(expm(S{1}*theta1)*expm(S{2}*theta2)*M3);
+T4 = simplify(expm(S{1}*theta1)*expm(S{2}*theta2)*expm(S{3}*theta3)*M4);
+T5 = simplify(expm(S{1}*theta1)*expm(S{2}*theta2)*expm(S{3}*theta3)*expm(S{4}*theta4)*M5);
 
-X_ref = [-0.02;0;-0.01];
+T6 = simplify(expm(S{6}*theta6)*M6);
+T7 = simplify(expm(S{6}*theta6)*expm(S{7}*theta7)*M7);
+T8 = simplify(expm(S{6}*theta6)*expm(S{7}*theta7)*expm(S{8}*theta8)*M8);
+T9 = simplify(expm(S{6}*theta6)*expm(S{7}*theta7)*expm(S{8}*theta8)*expm(S{9}*theta9)*M9);
 
-norm_e_left = 1;
-norm_e_right = 1; 
+T = {T1,T2,T3,T4,T5,T6,T7,T8,T9};
+m = {m1,m2,m3,m4,m5,m2,m3,m4,m5};
+% CoM calculation
+x_total = 0;
+y_total = 0;
+z_total = 0;
+m_total = 0;
+for i = 1:9
+    x_total = x_total + T{i}(1,4)*m{i};
+    y_total = y_total + T{i}(2,4)*m{i};
+    z_total = z_total + T{i}(3,4)*m{i};
+    m_total = m_total + m{i};
+end
+x_total = x_total/m_total;
+y_total = y_total/m_total;
+z_total = z_total/m_total;
+
+sym_X_act = [x_total;y_total;z_total];
+X_ref = [0;0;-0.135];
+sym_theta = [theta2 theta3 theta4 theta7 theta8 theta9];
+val_theta = [val_theta2 val_theta3 val_theta4 val_theta7 val_theta8 val_theta9];
+jacob = simplify(jacobian(sym_X_act,sym_theta))
+
+norm_e = 1;
 epsilon = 0.005;
 iteration = 0;
-jacob_left = simplify(jacobian(sym_X_act_left,sym_theta_left));
-jacob_right = simplify(jacobian(sym_X_act_right,sym_theta_right));
 
-while (norm_e_left > epsilon || norm_e_right > epsilon) && iteration < 100
-    val_X_act_left = vpa(subs(sym_X_act_left,[sym_theta_left sub_syms],[val_theta_left sub_vals]),8)
-    val_X_act_right = vpa(subs(sym_X_act_right,[sym_theta_right sub_syms],[val_theta_right sub_vals]),8)
-
-    error_X_left = vpa(X_ref - val_X_act_left,8);
-    error_X_right = vpa(X_ref - val_X_act_right,8);
-
-    norm_e_left = norm(error_X_left);
-    norm_e_right = norm(error_X_right);
-
-    val_jacob_left = vpa(subs(jacob_left,[sym_theta_left sub_syms],[val_theta_left sub_vals]),8);
-    val_jacob_right = vpa(subs(jacob_right,[sym_theta_right sub_syms],[val_theta_right sub_vals]),8);
-
-    inv_jacob_left = pinv(val_jacob_left);
-    inv_jacob_right = pinv(val_jacob_right);
-
-    delta_theta_left = inv_jacob_left*error_X_left;
-    delta_theta_right = inv_jacob_right*error_X_right;
-
-    val_theta_left = val_theta_left + delta_theta_left';
-    val_theta_left = mod(eval(val_theta_left) + pi, 2*pi) - pi;
-
-    val_theta_right = val_theta_right + delta_theta_right';
-    val_theta_right = mod(eval(val_theta_right) + pi, 2*pi) - pi;
-
+while norm_e > epsilon && iteration < 100
+    val_X_act = vpa(subs(sym_X_act,[sym_theta sub_syms],[val_theta sub_vals]),8);
+    error_X = vpa(X_ref - val_X_act,8);
+    norm_e = norm(error_X);
+    val_jacob = vpa(subs(jacob,[sym_theta sub_syms],[val_theta sub_vals]),8);
+    inv_jacob = pinv(val_jacob);
+    delta_theta = inv_jacob*error_X;
+    val_theta = val_theta + delta_theta';
+    val_theta = mod(eval(val_theta) + pi, 2*pi) - pi;
     iteration = iteration + 1;
 end
+val_theta
+val_X_act
 
-val_theta_left
-val_theta_right
-
-% CoM left leg Jacobian matrix for Orin
-% jacob(1,1) = - L2*cos(theta2) - L1*cos(theta1)*sin(theta2)
-% jacob(1,2) = 0
-% jacob(1,3) = 0
-% jacob(2,1) = -sin(theta3 + theta4)*(L2*sin(theta2) - L1*cos(theta1)*cos(theta2))
-% jacob(2,2) = L2*cos(theta2)*cos(theta3)*cos(theta4) - L1*cos(theta3)*sin(theta1)*sin(theta4) - L1*cos(theta4)*sin(theta1)*sin(theta3) - L2*cos(theta2)*sin(theta3)*sin(theta4) + L1*cos(theta1)*cos(theta3)*cos(theta4)*sin(theta2) - L1*cos(theta1)*sin(theta2)*sin(theta3)*sin(theta4)
-% jacob(2,3) = L4*cos(theta4) + L2*cos(theta2)*cos(theta3)*cos(theta4) - L1*cos(theta3)*sin(theta1)*sin(theta4) - L1*cos(theta4)*sin(theta1)*sin(theta3) - L2*cos(theta2)*sin(theta3)*sin(theta4) + L1*cos(theta1)*cos(theta3)*cos(theta4)*sin(theta2) - L1*cos(theta1)*sin(theta2)*sin(theta3)*sin(theta4)
-% jacob(3,1) = -cos(theta3 + theta4)*(L2*sin(theta2) - L1*cos(theta1)*cos(theta2))
-% jacob(3,2) = L1*sin(theta1)*sin(theta3)*sin(theta4) - L2*cos(theta2)*cos(theta3)*sin(theta4) - L2*cos(theta2)*cos(theta4)*sin(theta3) - L1*cos(theta3)*cos(theta4)*sin(theta1) - L1*cos(theta1)*cos(theta3)*sin(theta2)*sin(theta4) - L1*cos(theta1)*cos(theta4)*sin(theta2)*sin(theta3)
-% jacob(3,3) = L1*sin(theta1)*sin(theta3)*sin(theta4) - L1*cos(theta3)*cos(theta4)*sin(theta1) - L2*cos(theta2)*cos(theta3)*sin(theta4) - L2*cos(theta2)*cos(theta4)*sin(theta3) - L4*sin(theta4) - L1*cos(theta1)*cos(theta3)*sin(theta2)*sin(theta4) - L1*cos(theta1)*cos(theta4)*sin(theta2)*sin(theta3)
-
-% CoM right leg Jacobian matrix for Orin
-% jacob(1,1) = L1*cos(theta6)*sin(theta7) - L2*cos(theta7)
-% jacob(1,2) = 0
-% jacob(1,3) = 0
-% jacob(2,1) = -sin(theta8 + theta9)*(L2*sin(theta7) + L1*cos(theta6)*cos(theta7))
-% jacob(2,2) = L2*cos(theta7)*cos(theta8)*cos(theta9) + L1*cos(theta8)*sin(theta6)*sin(theta9) + L1*cos(theta9)*sin(theta6)*sin(theta8) - L2*cos(theta7)*sin(theta8)*sin(theta9) - L1*cos(theta6)*cos(theta8)*cos(theta9)*sin(theta7) + L1*cos(theta6)*sin(theta7)*sin(theta8)*sin(theta9)
-% jacob(2,3) = L4*cos(theta9) + L2*cos(theta7)*cos(theta8)*cos(theta9) + L1*cos(theta8)*sin(theta6)*sin(theta9) + L1*cos(theta9)*sin(theta6)*sin(theta8) - L2*cos(theta7)*sin(theta8)*sin(theta9) - L1*cos(theta6)*cos(theta8)*cos(theta9)*sin(theta7) + L1*cos(theta6)*sin(theta7)*sin(theta8)*sin(theta9)
-% jacob(3,1) = -cos(theta8 + theta9)*(L2*sin(theta7) + L1*cos(theta6)*cos(theta7))
-% jacob(3,2) = L1*cos(theta8)*cos(theta9)*sin(theta6) - L2*cos(theta7)*cos(theta8)*sin(theta9) - L2*cos(theta7)*cos(theta9)*sin(theta8) - L1*sin(theta6)*sin(theta8)*sin(theta9) + L1*cos(theta6)*cos(theta8)*sin(theta7)*sin(theta9) + L1*cos(theta6)*cos(theta9)*sin(theta7)*sin(theta8)
-% jacob(3,3) = L1*cos(theta8)*cos(theta9)*sin(theta6) - L4*sin(theta9) - L2*cos(theta7)*cos(theta8)*sin(theta9) - L2*cos(theta7)*cos(theta9)*sin(theta8) - L1*sin(theta6)*sin(theta8)*sin(theta9) + L1*cos(theta6)*cos(theta8)*sin(theta7)*sin(theta9) + L1*cos(theta6)*cos(theta9)*sin(theta7)*sin(theta8)
+% The jacobian matrix are for Orin computation
+% [ (cos(theta1)*(C33*m3*cos(theta2) - C31*m3*sin(theta2) - C41*m4*sin(theta2) - C51*m5*sin(theta2) + C43*m4*cos(theta2)*cos(theta3) - L4*m5*cos(theta2)*cos(theta3) + C42*m4*cos(theta2)*sin(theta3) + C53*m5*cos(theta2)*cos(theta3)*cos(theta4) + C52*m5*cos(theta2)*cos(theta3)*sin(theta4) + C52*m5*cos(theta2)*cos(theta4)*sin(theta3) - C53*m5*cos(theta2)*sin(theta3)*sin(theta4)))/(m1 + 2*m2 + 2*m3 + 2*m4 + 2*m5), -(m4*(C43*cos(theta3)*sin(theta1) + C42*sin(theta1)*sin(theta3) - C42*cos(theta1)*cos(theta3)*sin(theta2) + C43*cos(theta1)*sin(theta2)*sin(theta3)) + m5*(C53*cos(theta3)*cos(theta4)*sin(theta1) - L4*cos(theta3)*sin(theta1) + C52*cos(theta3)*sin(theta1)*sin(theta4) + C52*cos(theta4)*sin(theta1)*sin(theta3) - L4*cos(theta1)*sin(theta2)*sin(theta3) - C53*sin(theta1)*sin(theta3)*sin(theta4) - C52*cos(theta1)*cos(theta3)*cos(theta4)*sin(theta2) + C53*cos(theta1)*cos(theta3)*sin(theta2)*sin(theta4) + C53*cos(theta1)*cos(theta4)*sin(theta2)*sin(theta3) + C52*cos(theta1)*sin(theta2)*sin(theta3)*sin(theta4)))/(m1 + 2*m2 + 2*m3 + 2*m4 + 2*m5), -(m5*(C53*cos(theta3)*cos(theta4)*sin(theta1) + C52*cos(theta3)*sin(theta1)*sin(theta4) + C52*cos(theta4)*sin(theta1)*sin(theta3) - C53*sin(theta1)*sin(theta3)*sin(theta4) - C52*cos(theta1)*cos(theta3)*cos(theta4)*sin(theta2) + C53*cos(theta1)*cos(theta3)*sin(theta2)*sin(theta4) + C53*cos(theta1)*cos(theta4)*sin(theta2)*sin(theta3) + C52*cos(theta1)*sin(theta2)*sin(theta3)*sin(theta4)))/(m1 + 2*m2 + 2*m3 + 2*m4 + 2*m5),  (cos(theta6)*(C33*m3*cos(theta7) - C31*m3*sin(theta7) - C41*m4*sin(theta7) - C51*m5*sin(theta7) + C43*m4*cos(theta7)*cos(theta8) - L4*m5*cos(theta7)*cos(theta8) + C42*m4*cos(theta7)*sin(theta8) + C53*m5*cos(theta7)*cos(theta8)*cos(theta9) + C52*m5*cos(theta7)*cos(theta8)*sin(theta9) + C52*m5*cos(theta7)*cos(theta9)*sin(theta8) - C53*m5*cos(theta7)*sin(theta8)*sin(theta9)))/(m1 + 2*m2 + 2*m3 + 2*m4 + 2*m5), -(m4*(C43*cos(theta8)*sin(theta6) + C42*sin(theta6)*sin(theta8) - C42*cos(theta6)*cos(theta8)*sin(theta7) + C43*cos(theta6)*sin(theta7)*sin(theta8)) + m5*(C53*cos(theta8)*cos(theta9)*sin(theta6) - L4*cos(theta8)*sin(theta6) + C52*cos(theta8)*sin(theta6)*sin(theta9) + C52*cos(theta9)*sin(theta6)*sin(theta8) - L4*cos(theta6)*sin(theta7)*sin(theta8) - C53*sin(theta6)*sin(theta8)*sin(theta9) - C52*cos(theta6)*cos(theta8)*cos(theta9)*sin(theta7) + C53*cos(theta6)*cos(theta8)*sin(theta7)*sin(theta9) + C53*cos(theta6)*cos(theta9)*sin(theta7)*sin(theta8) + C52*cos(theta6)*sin(theta7)*sin(theta8)*sin(theta9)))/(m1 + 2*m2 + 2*m3 + 2*m4 + 2*m5), -(m5*(C53*cos(theta8)*cos(theta9)*sin(theta6) + C52*cos(theta8)*sin(theta6)*sin(theta9) + C52*cos(theta9)*sin(theta6)*sin(theta8) - C53*sin(theta6)*sin(theta8)*sin(theta9) - C52*cos(theta6)*cos(theta8)*cos(theta9)*sin(theta7) + C53*cos(theta6)*cos(theta8)*sin(theta7)*sin(theta9) + C53*cos(theta6)*cos(theta9)*sin(theta7)*sin(theta8) + C52*cos(theta6)*sin(theta7)*sin(theta8)*sin(theta9)))/(m1 + 2*m2 + 2*m3 + 2*m4 + 2*m5)]
+% [-(sin(theta1)*(C33*m3*cos(theta2) - C31*m3*sin(theta2) - C41*m4*sin(theta2) - C51*m5*sin(theta2) + C43*m4*cos(theta2)*cos(theta3) - L4*m5*cos(theta2)*cos(theta3) + C42*m4*cos(theta2)*sin(theta3) + C53*m5*cos(theta2)*cos(theta3)*cos(theta4) + C52*m5*cos(theta2)*cos(theta3)*sin(theta4) + C52*m5*cos(theta2)*cos(theta4)*sin(theta3) - C53*m5*cos(theta2)*sin(theta3)*sin(theta4)))/(m1 + 2*m2 + 2*m3 + 2*m4 + 2*m5), -(m4*(C43*cos(theta1)*cos(theta3) + C42*cos(theta1)*sin(theta3) + C42*cos(theta3)*sin(theta1)*sin(theta2) - C43*sin(theta1)*sin(theta2)*sin(theta3)) - m5*(L4*cos(theta1)*cos(theta3) - C53*cos(theta1)*cos(theta3)*cos(theta4) - C52*cos(theta1)*cos(theta3)*sin(theta4) - C52*cos(theta1)*cos(theta4)*sin(theta3) + C53*cos(theta1)*sin(theta3)*sin(theta4) - L4*sin(theta1)*sin(theta2)*sin(theta3) - C52*cos(theta3)*cos(theta4)*sin(theta1)*sin(theta2) + C53*cos(theta3)*sin(theta1)*sin(theta2)*sin(theta4) + C53*cos(theta4)*sin(theta1)*sin(theta2)*sin(theta3) + C52*sin(theta1)*sin(theta2)*sin(theta3)*sin(theta4)))/(m1 + 2*m2 + 2*m3 + 2*m4 + 2*m5), -(m5*(C53*cos(theta1)*cos(theta3)*cos(theta4) + C52*cos(theta1)*cos(theta3)*sin(theta4) + C52*cos(theta1)*cos(theta4)*sin(theta3) - C53*cos(theta1)*sin(theta3)*sin(theta4) + C52*cos(theta3)*cos(theta4)*sin(theta1)*sin(theta2) - C53*cos(theta3)*sin(theta1)*sin(theta2)*sin(theta4) - C53*cos(theta4)*sin(theta1)*sin(theta2)*sin(theta3) - C52*sin(theta1)*sin(theta2)*sin(theta3)*sin(theta4)))/(m1 + 2*m2 + 2*m3 + 2*m4 + 2*m5), -(sin(theta6)*(C33*m3*cos(theta7) - C31*m3*sin(theta7) - C41*m4*sin(theta7) - C51*m5*sin(theta7) + C43*m4*cos(theta7)*cos(theta8) - L4*m5*cos(theta7)*cos(theta8) + C42*m4*cos(theta7)*sin(theta8) + C53*m5*cos(theta7)*cos(theta8)*cos(theta9) + C52*m5*cos(theta7)*cos(theta8)*sin(theta9) + C52*m5*cos(theta7)*cos(theta9)*sin(theta8) - C53*m5*cos(theta7)*sin(theta8)*sin(theta9)))/(m1 + 2*m2 + 2*m3 + 2*m4 + 2*m5), -(m4*(C43*cos(theta6)*cos(theta8) + C42*cos(theta6)*sin(theta8) + C42*cos(theta8)*sin(theta6)*sin(theta7) - C43*sin(theta6)*sin(theta7)*sin(theta8)) - m5*(L4*cos(theta6)*cos(theta8) - C53*cos(theta6)*cos(theta8)*cos(theta9) - C52*cos(theta6)*cos(theta8)*sin(theta9) - C52*cos(theta6)*cos(theta9)*sin(theta8) + C53*cos(theta6)*sin(theta8)*sin(theta9) - L4*sin(theta6)*sin(theta7)*sin(theta8) - C52*cos(theta8)*cos(theta9)*sin(theta6)*sin(theta7) + C53*cos(theta8)*sin(theta6)*sin(theta7)*sin(theta9) + C53*cos(theta9)*sin(theta6)*sin(theta7)*sin(theta8) + C52*sin(theta6)*sin(theta7)*sin(theta8)*sin(theta9)))/(m1 + 2*m2 + 2*m3 + 2*m4 + 2*m5), -(m5*(C53*cos(theta6)*cos(theta8)*cos(theta9) + C52*cos(theta6)*cos(theta8)*sin(theta9) + C52*cos(theta6)*cos(theta9)*sin(theta8) - C53*cos(theta6)*sin(theta8)*sin(theta9) + C52*cos(theta8)*cos(theta9)*sin(theta6)*sin(theta7) - C53*cos(theta8)*sin(theta6)*sin(theta7)*sin(theta9) - C53*cos(theta9)*sin(theta6)*sin(theta7)*sin(theta8) - C52*sin(theta6)*sin(theta7)*sin(theta8)*sin(theta9)))/(m1 + 2*m2 + 2*m3 + 2*m4 + 2*m5)]
+% [                                -(m3*(C31*cos(theta2) + C33*sin(theta2)) + m5*(C51*cos(theta2) - L4*cos(theta3)*sin(theta2) + C53*cos(theta3)*cos(theta4)*sin(theta2) + C52*cos(theta3)*sin(theta2)*sin(theta4) + C52*cos(theta4)*sin(theta2)*sin(theta3) - C53*sin(theta2)*sin(theta3)*sin(theta4)) + m4*(C41*cos(theta2) + C43*cos(theta3)*sin(theta2) + C42*sin(theta2)*sin(theta3)))/(m1 + 2*m2 + 2*m3 + 2*m4 + 2*m5),                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          (cos(theta2)*(L4*m5*sin(theta3) + C52*m5*cos(theta3 + theta4) - C53*m5*sin(theta3 + theta4) + C42*m4*cos(theta3) - C43*m4*sin(theta3)))/(m1 + 2*m2 + 2*m3 + 2*m4 + 2*m5),                                                                                                                                                                                                                                                                                                                                (m5*cos(theta2)*(C52*cos(theta3 + theta4) - C53*sin(theta3 + theta4)))/(m1 + 2*m2 + 2*m3 + 2*m4 + 2*m5),                                 -(m3*(C31*cos(theta7) + C33*sin(theta7)) + m5*(C51*cos(theta7) - L4*cos(theta8)*sin(theta7) + C53*cos(theta8)*cos(theta9)*sin(theta7) + C52*cos(theta8)*sin(theta7)*sin(theta9) + C52*cos(theta9)*sin(theta7)*sin(theta8) - C53*sin(theta7)*sin(theta8)*sin(theta9)) + m4*(C41*cos(theta7) + C43*cos(theta8)*sin(theta7) + C42*sin(theta7)*sin(theta8)))/(m1 + 2*m2 + 2*m3 + 2*m4 + 2*m5),                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          (cos(theta7)*(L4*m5*sin(theta8) + C52*m5*cos(theta8 + theta9) - C53*m5*sin(theta8 + theta9) + C42*m4*cos(theta8) - C43*m4*sin(theta8)))/(m1 + 2*m2 + 2*m3 + 2*m4 + 2*m5),                                                                                                                                                                                                                                                                                                                                (m5*cos(theta7)*(C52*cos(theta8 + theta9) - C53*sin(theta8 + theta9)))/(m1 + 2*m2 + 2*m3 + 2*m4 + 2*m5)]
+% 
