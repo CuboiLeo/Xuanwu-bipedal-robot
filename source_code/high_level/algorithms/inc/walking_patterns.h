e219efbbf2749af41698745af8dc3bf7854b891b
CuboiLeo <yuxiliu0403@gmail.com>
2025-02-22 19:58:05 -0500

    feat(Orin): walking pattern started

--- source_code/high_level/algorithms/inc/walking_patterns.h
+++ source_code/high_level/algorithms/inc/walking_patterns.h
@@ -4,13 +4,28 @@
 #include <stdint.h>
 #include "robot_types.h"
 #include "robot_configs.h"
+#include "user_math.h"
+#include <math.h>
 
 class Walking_Patterns
 {
-    public:
+public:
+    
     Position computeLIPM(const Position &act_CoM_pos, const Velocity &act_CoM_vel);
 
+private:
+    double step_counter = 0; // Odd means left foot next, even means right foot next
+    Eigen::Vector5d sx;
+    Eigen::Vector5d sy;
+
+    static constexpr double zc = 0.4;                // CoM height
+    static constexpr double Tc = sqrt(zc / GRAVITY); // Time constant
+    static constexpr double Tsup = 0.8;              // Support phase time
+    static constexpr double C = cosh(Tsup / Tc);
+    static constexpr double S = sinh(Tsup / Tc);
+    static constexpr double a = 10; // Weighting factor of the evaluation function
+    static constexpr double b = 1;  // Weighting factor of the evaluation function
+    static constexpr double D = a * (C - 1) * (C - 1) + b * (S / Tc) * (S / Tc); // Evaluation function
 };
-    
 
 #endif
--- source_code/high_level/algorithms/src/walking_patterns.cpp
+++ source_code/high_level/algorithms/src/walking_patterns.cpp
@@ -1,6 +1,12 @@
 #include "walking_patterns.h"
 
+Walking_Patterns::Walking_Patterns()
+{
+    sx << 0, 0.3, 0.3, 0.3, 0;
+    sy << 0.2, 0.2, 0.2, 0.2, 0.2;
+}
+
 Position Walking_Patterns::computeLIPM(const Position &act_CoM_pos, const Velocity &act_CoM_vel)
 {
-    return act_CoM_pos;
+    
 }
