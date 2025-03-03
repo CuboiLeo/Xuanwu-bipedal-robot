b42ad2e227d520e8a48b496c16c5c0936afe8e06
CuboiLeo <yuxiliu0403@gmail.com>
2025-03-03 17:49:47 -0500

    feat(Orin): walking pattern using Kajita's LIPM tested, can now alternate between two legs, however, since there's no consideration for CoM transfer between legs, still cannot walk, need to implement CoM adjustment algorithm first

--- source_code/high_level/algorithms/inc/walking_patterns.h
+++ source_code/high_level/algorithms/inc/walking_patterns.h
@@ -6,16 +6,28 @@
 #include "robot_configs.h"
 #include "user_math.h"
 #include <math.h>
+#include <chrono>
 
 class Walking_Patterns
 {
 public:
-    Position computeLIPM(const Position &act_CoM_pos, const Velocity &act_CoM_vel, const Position &stance_foot_pos);
+    Walking_Patterns(){start_time = std::chrono::high_resolution_clock::now();};
+    Position gaitPlanner(const Position &act_CoM_pos, const Velocity &act_CoM_vel, const Position &stance_foot_pos);
+    double getStepCounter() { return step_counter; };
 
 private:
-    double step_counter = 0;                       // Odd means left foot next, even means right foot next
-    vector<double> sx = {0, 0.1, 0.1, 0.1, 0};     // nominal step offset in x (m)
-    vector<double> sy = {0.28, 0.28, 0.28, 0.28, 0.28}; // nominal step offset in y (m)
+    Position computeLIPM(const Position &act_CoM_pos, const Velocity &act_CoM_vel, const Position &stance_foot_pos);
+    Position generateFootTrajectory(const Position &next_step, const double &time);
+
+    Position next_step;
+    Position current_pos;
+    std::chrono::time_point<std::chrono::high_resolution_clock> start_time;
+    std::chrono::duration<double> duration;
+
+    double foot_lift = 0.04;                                 // Foot lift height
+    int step_counter = 0;                                 // Odd means right foot next, even means left foot next
+    std::vector<double> sx = {0.2, 0.2, 0.2, 0.2, 0.2};          // nominal step offset in x (m)
+    std::vector<double> sy = {0.28, 0.28, 0.28, 0.28, 0.28}; // nominal step offset in y (m)
     double x_local = 0;
     double y_local = 0;
     double xdot_local = 0;
@@ -29,7 +41,7 @@ private:
 
     static constexpr double zc = 0.4;                // CoM height
     static constexpr double Tc = sqrt(zc / GRAVITY); // Time constant
-    static constexpr double Tsup = 0.8;              // Support phase time
+    static constexpr double Tsup = 0.8;              // Single support phase time
     static constexpr double C = cosh(Tsup / Tc);
     static constexpr double S = sinh(Tsup / Tc);
     static constexpr double a = 10;                                              // Weighting factor of the evaluation function
