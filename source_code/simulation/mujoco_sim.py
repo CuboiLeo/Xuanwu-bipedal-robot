6351b21ada6f637756bf7384235bc6c300bbf87f
Leo Liu <65527170+CuboiLeo@users.noreply.github.com>
2025-03-09 16:53:29 -0400

    fix(Mujoco): fixed link position, inertia matrix, and mesh size, can now display normally in Mujoco

--- source_code/simulation/mujoco_sim.py
+++ source_code/simulation/mujoco_sim.py
@@ -1,10 +1,13 @@
-import os
-os.environ["MUJOCO_GL"] = "egl"
 import mujoco
 import mujoco.viewer as viewer
 import numpy as np
 
 m = mujoco.MjModel.from_xml_path("model/mjcf/Xuanwu.xml")
 d = mujoco.MjData(m)
+
+init_joint_pos = np.array([0.0, 0.0, 0.35, -0.5, 0.35, 0.0, 0.0, 0.35, -0.5, 0.35])
+init_base_pos = np.array([0, 0, 1])
+init_base_eular_zyx = np.array([0.0, 0.0, 0.0])
+imu_eular_bias = np.array([0.0, 0.0, 0.0])
 
 viewer.launch(m,d)
--- source_code/simulation/urdf2xml.py
+++ source_code/simulation/urdf2xml.py
@@ -1,3 +1,3 @@
 import mujoco
 model = mujoco.MjModel.from_xml_path("model/urdf/Xuanwu.urdf")
-mujoco.mj_saveLastXML("model/mjcf/Xuanwu.xml", model)
+mujoco.mj_saveLastXML("model/Xuanwu.xml", model)
